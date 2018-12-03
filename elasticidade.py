a58fedf4e06de5372f0239de9768d8cb21a9802e
Vitor Araujo <vitorgamer58@outlook.com>
2018-12-03 11:35:35 -0200

    Update elasticidade.py

--- elasticidade.py
+++ elasticidade.py
@@ -1,3 +1,4 @@
+# python version ??  tested with http://mathcs.holycross.edu/~kwalsh/python/
 precoinicial = input("Digite o preco inicial: ")
 precofinal = input("Gigite o preco final: ")
 variavel1 = (precofinal-precoinicial)/(precoinicial)*100
@@ -6,8 +7,10 @@ demandafinal = input ("Digite a quantidade final demandada: ")
 variavel2 = (demandafinal-demandainicial)/(demandainicial)*100
 print variavel1
 print variavel2
-resultado = abs(variavel2 / variavel1)
-
-
-
-print('A eslasticidade final e de: ', resultado)
+elas = (variavel2 / variavel1)
+if elas == 1:
+	print('A demanda do produto e unitaria e seu valor e: ', elas)
+if elas > 1:
+	print('A demanda e elestica e seu valor e: ', elas)
+if elas <1:
+	print('A demanda e inelestica e seu valor e: ', elas)
